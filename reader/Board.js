ae957dc4606af57d1b7f3b3b154eb0798fcc4bdf
joaosilva22 <j.pedro004@gmail.com>
2016-12-23 13:07:28 +0000

    Refactor board

--- reader/Board.js
+++ reader/Board.js
@@ -1,24 +1,96 @@
+class Cell extends CGFobject {
+    constructor(scene, type, position) {
+		super(scene);
+		this.side = new Rectangle(scene, 0.25, 0, -0.25, -1);
+		this.hex = new Hexagon(scene);
 
-class Board {
-    constructor(scene) {
-		this.hex = new HexaPrism(scene);
-		this.board = [];
-		
-		this.n = new Connection();
-		this.n.getBoardRequest(this);
-		
+		this.type = type;
+		this.position = position;
+	}
+
+	display(textures) {
+		let initial = Math.PI/6;
+		for (let i = 0; i < 6; i++) {
+			let angle = Math.PI/6 + i*Math.PI/3;
+			this.scene.pushMatrix();
+			this.scene.rotate(angle, 0, 1, 0);
+			this.scene.translate(0, 0, Math.sqrt(3)/4);
+			textures['base'].bind();
+			this.side.display();
+			this.scene.popMatrix();
+		}
+
+		this.scene.pushMatrix();
+		textures[this.type].bind();
+		this.hex.display();
+		this.scene.popMatrix();
+    }
+}
+
+class Ship extends CGFobject {
+	constructor(scene, faction, position) {
+		super(scene);
+		this.geometry = new Cylinder(scene, 0.2, 0, 1, 6, 6);
 		this.scene = scene;
+
+		this.faction = faction;
+		this.position = position;
+	}
+
+	display(textures) {
+		this.scene.pushMatrix();
+		this.scene.rotate(-Math.PI/2, 1, 0, 0);
+		textures[this.faction].bind();
+		this.geometry.display();
+		this.scene.popMatrix();
+	}
+}
+
+class Board extends CGFobject{
+    constructor(scene) {
+		super(scene);
+		this.connection = new Connection();
+
+		this.board = [];
+		this.cells = [];
+		this.ships = [];
+		
+		this.connection.getBoardRequest(this);
 		this.distance = Math.sqrt(3)/2;
 
-		this.objects = [];
+		this.textures = {
+			'factionOne' : new CGFtexture(this.scene, 'resources/relaig/base.png'),
+			'factionTwo' : new CGFtexture(this.scene, 'resources/relaig/base.png'),
+			'base' : new CGFtexture(this.scene, 'resources/relaig/base.png'),
+			'0': new CGFtexture(this.scene, 'resources/relaig/empty.png'),
+			'1': new CGFtexture(this.scene, 'resources/relaig/one-planet.png'),
+			'2': new CGFtexture(this.scene, 'resources/relaig/two-planet.png'),
+			'3': new CGFtexture(this.scene, 'resources/relaig/three-planet.png'),
+			'g': new CGFtexture(this.scene, 'resources/relaig/blue-home.png'),
+			'h': new CGFtexture(this.scene, 'resources/relaig/yellow-home.png'),
+			'z': new CGFtexture(this.scene, 'resources/relaig/red-nebula.png'),
+			'w': new CGFtexture(this.scene, 'resources/relaig/wormhole.png'),
+			'b': new CGFtexture(this.scene, 'resources/relaig/blackhole.png')
+		}
     }
 
 	initBoard() {
+		this.cells = [];
+		this.ships = [];
+
 		for (let i = 0; i < this.board.length; i++) {
 			let line = this.board[i];
-			this.objects.push([]);
 			for (let j = 0; j < line.length; j++) {
-				this.objects[i].push(new HexaPrism(this.scene));
+				if (this.getMapLayer(this.board[i][j]) !== 'v') {
+					this.cells.push(new Cell(this.scene, this.getMapLayer(this.board[i][j]), {x: j, z: i}));
+				}
+				
+				if (this.getMovementLayer(this.board[i][j]) === 'A') {
+					this.ships.push(new Ship(this.scene, 'factionOne', {x: j, z: i}));
+				}
+				if (this.getMovementLayer(this.board[i][j][0]) === 'B') {
+					this.ships.push(new Ship(this.scene, 'factionTwo', {x: j, z: i}));
+				}
 			}
 		}
 	}
@@ -27,35 +99,47 @@ class Board {
 		return cell[1];
     }
 
+	getMovementLayer(cell) {
+		return cell[0];
+	}
+
     display() {
-		if (!this.board) {
-			return;
-		}
+		let self = this;
+		
+		this.cells.forEach(function(cell) {
+			self.scene.pushMatrix();
+			self.scene.scale(5, 5, 5);
+			self.scene.translate(-5.5 * self.distance, 0, -3.5 * self.distance);
 
-		for (let i = 0; i < this.board.length; i++) {
-			let line = this.board[i];
-			for (let j = 0; j < line.length; j++) {
-				let cell = line[j];
-				if (this.getMapLayer(cell) !== 'v') {
-					this.scene.pushMatrix();
-
-					this.scene.scale(5, 5, 5);
-
-					this.scene.translate(-5.5 * this.distance, 0, -3.5 * this.distance);
-
-					if (i % 2 === 0) {
-						let offset = i * this.distance / 2;
-						this.scene.translate(j * this.distance + offset, this.objects[i][j].verticalOffset, i * 0.75);
-					} else {
-						let offset = (i * this.distance - this.distance) / 2;
-						this.scene.translate(j * this.distance + this.distance / 2 + offset, this.objects[i][j].verticalOffset, i * 0.75);
-					}
-					
-					this.objects[i][j].display(this.getMapLayer(cell));
-					this.scene.popMatrix();
-				}
+			if (cell.position.z % 2 === 0) {
+				let offset = cell.position.z * self.distance / 2;
+				self.scene.translate(cell.position.x * self.distance + offset, 0, cell.position.z * 0.75);
+			} else {
+				let offset = (cell.position.z * self.distance - self.distance) / 2;
+				self.scene.translate(cell.position.x * self.distance + self.distance / 2 + offset, 0, cell.position.z * 0.75);
 			}
-		}
+
+			cell.display(self.textures);
+			self.scene.popMatrix();
+		});
+		
+		this.ships.forEach(function(ship) {
+			self.scene.pushMatrix();
+			self.scene.scale(5, 5, 5);
+			self.scene.translate(-5.5 * self.distance, 0, -3.5 * self.distance);
+			
+			if (ship.position.z % 2 === 0) {
+				let offset = ship.position.z * self.distance / 2;
+				self.scene.translate(ship.position.x * self.distance + offset, 0, ship.position.z * 0.75);
+			} else {
+				let offset = (ship.position.z * self.distance - self.distance) / 2;
+				self.scene.translate(ship.position.x * self.distance + self.distance / 2 + offset, 0, ship.position.z * 0.75);
+			}
+
+			ship.display(self.textures);
+			self.scene.popMatrix();
+		});
+		
     }
 
 	setTexCoords() {};
--- reader/Hexagon.js
+++ reader/Hexagon.js
@@ -42,45 +42,4 @@ class Hexagon extends CGFobject {
     }
 }
 
-class HexaPrism extends CGFobject {
-    constructor(scene) {
-		super(scene);
 
-		this.side = new Rectangle(scene, 0.25, 0, -0.25, -1);
-		this.hex = new Hexagon(scene);
-
-		this.verticalOffset = 0;//Math.random() * 0.10;
-
-		this.textures = {
-			'base' : new CGFtexture(this.scene, 'resources/relaig/base.png'),
-			'0': new CGFtexture(this.scene, 'resources/relaig/empty.png'),
-			'1': new CGFtexture(this.scene, 'resources/relaig/one-planet.png'),
-			'2': new CGFtexture(this.scene, 'resources/relaig/two-planet.png'),
-			'3': new CGFtexture(this.scene, 'resources/relaig/three-planet.png'),
-			'g': new CGFtexture(this.scene, 'resources/relaig/blue-home.png'),
-			'h': new CGFtexture(this.scene, 'resources/relaig/yellow-home.png'),
-			'z': new CGFtexture(this.scene, 'resources/relaig/red-nebula.png'),
-			'w': new CGFtexture(this.scene, 'resources/relaig/wormhole.png'),
-			'b': new CGFtexture(this.scene, 'resources/relaig/blackhole.png')
-		}
-			
-	}
-
-	display(type) {
-		let initial = Math.PI/6;
-		for (let i = 0; i < 6; i++) {
-			let angle = Math.PI/6 + i*Math.PI/3;
-			this.scene.pushMatrix();
-			this.scene.rotate(angle, 0, 1, 0);
-			this.scene.translate(0, 0, Math.sqrt(3)/4);
-			this.textures['base'].bind();
-			this.side.display();
-			this.scene.popMatrix();
-		}
-
-		this.scene.pushMatrix();
-		this.textures[type].bind();
-		this.hex.display();
-		this.scene.popMatrix();
-    }
-}
